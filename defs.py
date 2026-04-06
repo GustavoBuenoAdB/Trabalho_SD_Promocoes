6906d9447f0cf9eb94525652403965fed51218d0
GustavoBuenoAdB <gustavobuenodacosta@gmail.com>
2026-04-05 21:50:12 -0300

    fé

--- defs.py
+++ defs.py
@@ -11,8 +11,26 @@ PROM = 3
 NOTI = 4
 
 CHAVE_PUBLICA = {
-    GATE: "pub_gate.der",
-    RANK: "pub_rank.der",
-    PROM: "pub_prom.der",
-    NOTI: "pub_noti.der",
+	GATE: "pub_gate.der",
+	RANK: "pub_rank.der",
+	PROM: "pub_prom.der",
+	NOTI: "pub_noti.der",
+}
+
+PROMOCAO_LIVRO = 1
+PROMOCAO_ROUPA = 2
+PROMOCAO_ESPORTE = 3
+PROMOCAO_DOMESTICO = 4
+PROMOCAO_COMIDA = 5
+PROMOCAO_QUENTES = 6
+
+
+CABECALHO = {
+	ini_bits_SHA: 0
+	tam_bits_SHA: 32
+	id_promo: 
+	voto:
+	promo
+	(nome, preço, topicos)
+
 }
